ab1075e2d3f31671376962c790dcb834e950d436
Maxim Pshater <lemaxw@gmail.com>
2024-06-28 14:55:02 +0300

    before the move to new computer

--- backup.sh
+++ backup.sh
@@ -3,3 +3,5 @@
 docker exec -it images_management-db-1 pg_dump -U admin -d russian_poetry > backup.sql
 
 docker exec -it images_management-db-1 pg_dump -U admin -d ukranian_poetry > backup_ua.sql
+
+docker exec -it images_management-db-1 pg_dump -U admin -d english_poetry > backup_en.sql
